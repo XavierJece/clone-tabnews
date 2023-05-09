630f0a5d4bea0453fe1052ebf5660aa323262756
Jecé Xavier <jece@alunos.utfpr.edu.br>
2023-05-09 23:18:34 +0000

    build: add first page

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+function Home() {
+  return (
+    <>
+      <h1>Lembre-se que você é o protagonista da tua vida!</h1>
+      <p>
+        Você pode ter momento bom e ruim, e com tempo vai aprender que quando
+        estiver em tempos bons tem que aproveitar e quando estiver em baixo, vai
+        agir para voltar a subir, só depende de ti!
+      </p>
+      <p>
+        Tenha fé em ti e nas coisas que faz, tenho certeza que vai conseguir!
+      </p>
+      <strong>
+        Para finalizar EU TE AMO!
+      </strong>
+    </>
+  );
+}
+
+export default Home;
