60058ffde9a12adbae03cf5ee276a23da72f07b4
Jecé Xavier Pereira Neto <jece@alunos.utfpr.edu.com.br>
2023-12-09 10:35:09 -0300

    feat: get database version

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
-import database from 'infra/database.js';
+import database from 'infra/database';
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 +1 as sun;");
-  console.log(result.rows);
-  response.status(200).json({ status: "ok" });
+  const updateAt = new Date().toISOString();
+  
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+  response.status(200).json({ 
+    updated_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue)
+      }
+    }
+  });
 }
 
 export default status;
--- pages/tests/integration/api/v1/status/get.test.js
+++ pages/tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {  
   const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toEqual(200);
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json(); 
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+
 });
