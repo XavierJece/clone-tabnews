5d5e7c959138108933d772319556f1cdfa73e508
Jecé Xavier Pereira Neto <jece@alunos.utfpr.edu.com.br>
2023-12-19 07:30:08 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,10 +6,14 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
+  const databaseName = process.env.POSTGRES_DB;
   const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
   const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';");
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
 
   const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
 
@@ -19,7 +23,7 @@ async function status(request, response) {
       database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: databaseOpenedConnectionsValue, 
+        opened_connections: parseInt(databaseOpenedConnectionsValue), 
       }
     }
   });
