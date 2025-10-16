21e351df20e757d08ce5a5ab07962aea67b6c3eb
Jecé Xavier <jecexavier12@gmail.com>
2025-10-16 17:56:24 -0300

    feat: send activation email after `user` registration

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -13,8 +13,8 @@ async function postHandler(request, response) {
   const userInputValues = request.body;
   const newUser = await user.create(userInputValues);
 
-  //1. Criar token de ativação
-  await activation.sendEmailToUser(newUser);
+  const activationToken = await activation.create(newUser.id);
+  await activation.sendEmailToUser(newUser, activationToken);
 
   return response.status(201).json(newUser);
 }
